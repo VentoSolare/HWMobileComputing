2ec6aa8c3c76762429a8e58f408c53a62171f9ad
Auri Pykäläinen <auri.pykalainen@student.oulu.fi>
2025-02-16 17:41:46 +0200

    HW4

--- app/src/main/java/com/example/composetutorial/SecondaryView.kt
+++ app/src/main/java/com/example/composetutorial/SecondaryView.kt
@@ -1,15 +1,14 @@
 package com.example.composetutorial
 
+import android.Manifest
 import android.content.Context
+import android.content.Intent
 import android.net.Uri
-import android.os.Environment
-import android.provider.OpenableColumns
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -18,6 +17,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
@@ -27,6 +27,8 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
@@ -39,14 +41,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.customview.widget.Openable
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavHostController
-import coil3.compose.rememberAsyncImagePainter
+import coil.compose.rememberAsyncImagePainter
 import java.io.File
-import java.io.FileOutputStream
 import java.io.InputStream
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -55,6 +56,21 @@ fun SecondaryView(navController: NavHostController) {
     val context = LocalContext.current
     var username by remember { mutableStateOf(loadUsername(context)) }
     var imageUri by remember { mutableStateOf(loadImageUri(context)) }
+    var notificationsAllowed by remember { mutableStateOf(loadNotificationPreference(context)) }
+
+    val notificationPermissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            startTemperatureMonitorService(context)
+            notificationsAllowed = true
+            saveNotificationPreference(context, true)
+        } else {
+            notificationsAllowed = false
+            saveNotificationPreference(context, false)
+        }
+    }
+
 
     val imagePicker = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.GetContent()
@@ -102,7 +118,6 @@ fun SecondaryView(navController: NavHostController) {
 
             Spacer(modifier = Modifier.height(16.dp))
 
-            // Display Image or Placeholder
             imageUri?.let { uri ->
                 Image(
                     painter = rememberAsyncImagePainter(uri),
@@ -116,41 +131,81 @@ fun SecondaryView(navController: NavHostController) {
                 modifier = Modifier
                     .size(120.dp)
                     .clip(CircleShape)
-                    .background(Color.Gray)
+                    .background(Color.Black)
                     .clickable { imagePicker.launch("image/*") },
                 contentAlignment = Alignment.Center
             ) {
-                Text("Pick Image", color = Color.White)
+                Text("Tap to pick image", color = Color.White, textAlign = TextAlign.Center)
             }
 
             Spacer(modifier = Modifier.height(16.dp))
 
-            // Username Input Field
             OutlinedTextField(
                 value = username,
-                onValueChange = {
-                    username = it
-                    //saveUsername(context, it)
-                },
+                onValueChange = { username = it },
                 label = { Text("Enter username", style = MaterialTheme.typography.titleMedium) },
                 singleLine = true,
                 keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done),
                 keyboardActions = KeyboardActions(
                     onDone = {
-                        if(username.isBlank()) {
-                            username = "Guest"
-                            saveUsername(context, "Guest")
+                        if (username.isBlank()) {
+                            username = "Anonymous user"
+                            saveUsername(context, "Anonymous user")
                         } else {
                             saveUsername(context, username)
                         }
                     }
                 )
             )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Text("Enable Notifications")
+                Spacer(modifier = Modifier.width(8.dp))
+                Switch(
+                    checked = notificationsAllowed,
+                    onCheckedChange = { isChecked ->
+                        notificationsAllowed = isChecked
+                        saveNotificationPreference(context, isChecked)
+                        if (isChecked) {
+                            notificationPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+                        } else {
+                            context.stopService(
+                                Intent(
+                                    context,
+                                    TemperatureMonitorService::class.java
+                                )
+                            )
+                        }
+                    },
+                    colors = SwitchDefaults.colors(checkedTrackColor = Color.Black, uncheckedTrackColor = Color.Black)
+                )
+            }
         }
     }
 }
 
-// Save & Load Functions
+fun startTemperatureMonitorService(context: Context) {
+    ContextCompat.startForegroundService(
+        context,
+        Intent(context, TemperatureMonitorService::class.java)
+    )
+}
+
+fun loadNotificationPreference(context: Context): Boolean {
+    val sharedPref = context.getSharedPreferences("app_prefs", Context.MODE_PRIVATE)
+    return sharedPref.getBoolean("notifications_allowed", false)
+}
+
+fun saveNotificationPreference(context: Context, allowed: Boolean) {
+    val sharedPref = context.getSharedPreferences("app_prefs", Context.MODE_PRIVATE)
+    sharedPref.edit().putBoolean("notifications_allowed", allowed).apply()
+}
+
 private fun saveUsername(context: Context, username: String) {
     val prefs = context.getSharedPreferences("user_prefs", Context.MODE_PRIVATE)
     prefs.edit().putString("username", username).apply()
@@ -158,7 +213,7 @@ private fun saveUsername(context: Context, username: String) {
 
 fun loadUsername(context: Context): String {
     val prefs = context.getSharedPreferences("user_prefs", Context.MODE_PRIVATE)
-    return prefs.getString("username", "Guest") ?: "Guest"  // Default username
+    return prefs.getString("username", "Anonymous user") ?: "Anonymous user"
 }
 
 private fun saveImageUri(context: Context, uri: Uri) {
@@ -173,18 +228,23 @@ fun loadImageUri(context: Context): Uri? {
 }
 
 private fun storageImage(context: Context, uri: Uri): Uri? {
-    val inputStream: InputStream = context.contentResolver.openInputStream(uri) ?: return null
+    val contentResolver = context.contentResolver
+    val inputStream: InputStream? = contentResolver.openInputStream(uri)
+    if (inputStream != null) {
 
-    val picturesDir = File(context.getExternalFilesDir(Environment.DIRECTORY_PICTURES), "ComposeTutorial")
-    if (!picturesDir.exists()) picturesDir.mkdirs()  // Ensure directory exists
+        val outputFile = File(context.filesDir, "profile_image.jpg")
 
-    val fileName = "profile_image.jpg"
-    val outputFile = File(picturesDir, fileName)
-
-    inputStream.use { input ->
-        FileOutputStream(outputFile).use { output ->
-            input.copyTo(output)
+        inputStream.use { input ->
+            outputFile.outputStream().use { output ->
+                input.copyTo(output)
+            }
         }
+
+        return Uri.fromFile(outputFile)
+    } else {
+        return null
     }
-    return Uri.fromFile(outputFile)
 }
+
+
+
